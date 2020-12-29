7b8651584fb412a02cddeb3920514ed800627372
Yu Lin <linyu3924@gmail.com>
2020-12-29 15:31:15 -0500

    implemented apollo graphql middleware, and successfully connected to server, and database

--- capture-the-flag/service/PlayerService.swift
+++ capture-the-flag/service/PlayerService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 import Apollo
 
-
+// This is A SINGLETON.
 class Network {
   static let shared = Network()
   private(set) lazy var apollo = ApolloClient(url: URL(string: "http://" + localhost + ":8080/query")!)
@@ -22,47 +22,9 @@ func getPlayerLocation () -> () {
     print(mutation.queryDocument)
     print(net)
 
-    // The Network's apollo is a shared singleton. It can be used anywhere.
+    // The Network's apollo is a shared singleton.
     Network.shared.apollo.perform(mutation: CreateChannelMutation()){ result in
         guard let data = try? result.get().data else {return}
       print(data)
     }
 }
-
-//    let url = URL(string: "http://" + localhost + ":8080/query")!
-//    var request = URLRequest(url: url)
-//    request.httpMethod = "POST"
-//
-//
-//    // HTTP Request Parameters which will be sent in HTTP Request Body
-//    let postString =
-//    """
-//
-//       mutation
-//        createChannel {
-//           createChannel(
-//           input:
-//               {channel:
-//               {name: "Channel 1",
-//                 platform:
-//                 {name: "Platform for channel 1"}
-//               }
-//             })
-//               {
-//                 id
-//                 name
-//                 platform {
-//                   name
-//                 }
-//               }
-//       }
-//    """
-//    // Set HTTP Request Body
-//    request.httpBody = postString.data(using: String.Encoding.utf8);
-//
-//    NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) {(response, data, error) in
-//        guard let data = data else { return }
-//        print(String(data: data, encoding: .utf8)!)
-    
-
-
